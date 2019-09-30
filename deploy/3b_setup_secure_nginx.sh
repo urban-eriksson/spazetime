eb5feb37df85fd9701a3b638ad927e6da2f0173e
Urban Eriksson <urban-eriksson@users.noreply.github.com>
2019-09-30 21:31:06 +0200

    Update 3b_setup_secure_nginx.sh

--- deploy/3b_setup_secure_nginx.sh
+++ deploy/3b_setup_secure_nginx.sh
@@ -7,31 +7,19 @@ site_location="/etc/nginx/sites-available/spazetime.org"
 # Create the Nginx server block file:
 sudo tee $site_location > /dev/null <<EOF 
 
-
-server {
-        listen 443 ssl http2;
-        listen [::]:443 ssl http2;
-        include snippets/self-signed.conf;
-        include snippets/ssl-params.conf;
-
-
-        root /home/ubuntu/spazetime/spazetime-frontend/dist/spazetime-frontend;
-
-        location / {
-            try_files \$uri\$args \$uri\$args/ /index.html;
-        }
-
-}
-
 server {
     listen 80;
     listen [::]:80;
 
-    server_name _;
+    # server_name _;
+    server_name spazetime.org www.spazetime.org;
+    
+    root /home/ubuntu/spazetime/spazetime-frontend/dist/spazetime-frontend;
 
-    return 301 https://\$host\$request_uri;
+    location / {
+        try_files \$uri\$args \$uri\$args/ /index.html;
+    }
 }
-
 
 EOF
 
@@ -45,3 +33,4 @@ sudo systemctl restart nginx
 # Let's encrypt
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt install python-certbot-nginx
+sudo certbot --nginx -d spazetime.org -d www.spazetime.org
