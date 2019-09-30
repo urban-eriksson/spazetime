9c30af47cb3c5961e5eac9f597720df6196da7da
Urban Eriksson <urban-eriksson@users.noreply.github.com>
2019-09-30 21:59:33 +0200

    Create 3_setup_nginx.sh

---
+++ deploy/3_setup_nginx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# NGINX server setup
+sudo apt install -y nginx
+site_location="/etc/nginx/sites-available/spazetime.org"
+
+# Create the Nginx server block file:
+sudo tee $site_location > /dev/null <<EOF
+
+server {
+    listen 80;
+    listen [::]:80;
+
+    server_name spazetime.org www.spazetime.org;
+    
+    root /home/ubuntu/spazetime/spazetime-frontend/dist/spazetime-frontend;
+
+    location / {
+        try_files \$uri\$args \$uri\$args/ /index.html;
+    }
+}
+
+EOF
+
+# Link to make it available
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s $site_location /etc/nginx/sites-enabled/
+
+# Restart nginx
+sudo systemctl restart nginx
