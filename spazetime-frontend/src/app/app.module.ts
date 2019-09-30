48e6354965c60d02b58ef46409f81e0ca12e6dae
Urban Eriksson <123ube@gmail.com>
2019-09-30 23:06:17 +0200

    misc fixes

--- spazetime-frontend/src/app/app.module.ts
+++ spazetime-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { AboutComponent } from './about/about.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [
--- spazetime-frontend/src/app/spazetime/spazetime.component.ts
+++ spazetime-frontend/src/app/spazetime/spazetime.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-spazetime',
   templateUrl: './spazetime.component.html',
   styleUrls: ['./spazetime.component.scss']
 })
-export class SpazetimeComponent implements OnInit {
-  utcTime: Date;
+export class SpazetimeComponent implements OnInit, OnDestroy {
+  solarTime: Date;
   spazeSign: string;
   spazeHours: number;
   spazeMinutes: number;
   spazeSeconds: number;
+  interval: any;
 
 
   constructor() { }
 
   ngOnInit() {
-    // this.startUtcTime();
+    this.getTimeAndLocation();
+    this.startTimeLocationLoop();
   }
 
-  startUtcTime(): void {
-    setInterval(() => {
-      this.utcTime = new Date();
-      console.log(this.utcTime);
+  startTimeLocationLoop() {
+    this.interval = setInterval(() => {
+      this.getTimeAndLocation();
     }, 1000);
 
   }
 
-  getLocation(): void{
+  getTimeAndLocation(): void{
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
           console.log(position);
@@ -36,21 +37,27 @@ export class SpazetimeComponent implements OnInit {
           const utcDate = (new Date()).getTime();
           console.log(utcDate);
           const localDate = utcDate - spaze * 60000 * 60;
-          this.utcTime = new Date(localDate);
+          this.solarTime = new Date(localDate);
 
-          if (position.coords.longitude > 0) {
+          if (spaze < 0) {
             this.spazeSign = '-';
           } else {
             this.spazeSign = ' ';
           }
-          const absSpaze = Math.abs(position.coords.longitude / 15);
+
+          const absSpaze = Math.abs(spaze);
           this.spazeHours = Math.floor(absSpaze);
           this.spazeMinutes = Math.floor((absSpaze - this.spazeHours) * 60);
           this.spazeSeconds = Math.floor(((absSpaze - this.spazeHours) * 60 - this.spazeMinutes) * 60);
+
         });
     } else {
        console.log('No support for geolocation');
     }
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
 }
