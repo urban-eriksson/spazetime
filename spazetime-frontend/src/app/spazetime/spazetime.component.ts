64f234eace339233def324b53f462c0feac190cf
Urban Eriksson <123ube@gmail.com>
2019-10-17 22:28:45 +0200

    equation-of-time

--- spazetime-frontend/src/app/spazetime/spazetime.component.ts
+++ spazetime-frontend/src/app/spazetime/spazetime.component.ts
@@ -28,12 +28,39 @@ export class SpazetimeComponent implements OnInit, OnDestroy {
 
   }
 
+  isLeapYear(date: Date) {
+    const year = date.getUTCFullYear();
+    if ((year & 3) !== 0) {
+       return false;
+    } else {
+      return ((year % 100) !== 0 || (year % 400) === 0);
+    }
+  };
+
+
   getTimeAndLocation(): void {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
           console.log(position);
 
           const spaze = - position.coords.longitude / 15;
+          const now = new Date();
+          const start = new Date(now.getFullYear(), 0, 0);
+          const delta = now.getTime() - start.getTime();
+          const oneDay = 1000 * 60 * 60 * 24;
+          const dayFraction = delta / oneDay;
+          let gamma = 0;
+          if (this.isLeapYear(now)) {
+            gamma = 2 * Math.PI / 366 * (dayFraction - 0.5);
+          } else {
+            gamma = 2 * Math.PI / 365 * (dayFraction - 0.5);
+          }
+          const eqtime = 229.18 * (0.000075 + 0.001868 * Math.cos(gamma)
+           - 0.032077 * Math.sin(gamma)
+           - 0.014615 * Math.cos(2 * gamma)
+           - 0.040849 * Math.sin(2 * gamma));
+          console.log(dayFraction);
+          console.log(eqtime);
           const utcDate = (new Date()).getTime();
           console.log(utcDate);
           const localDate = utcDate - spaze * 60000 * 60;
