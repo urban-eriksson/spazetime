4a4600867dedf0d85dcee7da74591771196df61d
Urban Eriksson <urban-eriksson@users.noreply.github.com>
2019-10-18 23:04:58 +0200

    Update 3_setup_nginx.sh

--- deploy/3_setup_nginx.sh
+++ deploy/3_setup_nginx.sh
@@ -2,7 +2,8 @@
 
 # NGINX server setup
 sudo apt install -y nginx
-site_location="/etc/nginx/sites-available/spazetime.org"
+# site_location="/etc/nginx/sites-available/spazetime.org"
+site_location="/etc/nginx/sites-available/localsolartime.org"
 
 # Create the Nginx server block file:
 sudo tee $site_location > /dev/null <<EOF
@@ -11,6 +12,7 @@ server {
     listen 80;
     listen [::]:80;
 
+    # server_name spazetime.org www.spazetime.org;
     server_name localsolartime.org www.localsolartime.org;
     
     root /home/ubuntu/spazetime/spazetime-frontend/dist/spazetime-frontend;
